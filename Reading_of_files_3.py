42994687a3ffec59289de25adde59f86023e564a
Марк <fenster.mark@yandex.ru>
2023-01-28 21:58:26 +0200

    created class FilesProcessor

--- Reading_of_files_3.py
+++ Reading_of_files_3.py
@@ -1,17 +1,21 @@
-def create_files_dict(file_1, file_2, file_3):
-    
-    files_list = [file_1, file_2, file_3]
-    files_dict = {}
-    
-    for file in files_list:
-        
-        with open(file, encoding='utf-8') as file_in_the_process:
-            strings_quantity = len(file_in_the_process.readlines())
-        with open(file, encoding='utf-8') as file_in_the_process:
-             content = file_in_the_process.read()
-            
-        files_dict[file] = [strings_quantity, content]
-    
-    return files_dict
+class FilesProcessor:
+    def __init__(self, files_names_list):
+        self.files_names_list = files_names_list
 
-print(create_files_dict('1.txt', '2.txt', '3.txt'))
+    def create_files_dict(self):
+        
+        files_dict = {}
+        
+        for file in self.files_names_list:
+            
+            with open(file, encoding='utf-8') as file_in_the_process:
+                strings_quantity = len(file_in_the_process.readlines())
+            with open(file, encoding='utf-8') as file_in_the_process:
+                content = file_in_the_process.read()
+                
+            files_dict[file] = [strings_quantity, content]
+        
+        return files_dict
+
+processor = FilesProcessor(['1.txt', '2.txt', '3.txt'])
+print(processor.create_files_dict())
